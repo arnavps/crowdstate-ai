b158791e2785e74606259f380fc79c980227c56c
arnavps <arnps1806@gmail.com>
2026-03-24 22:03:26 +0530

    changes+3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import DashboardLayout from "./components/Layout/DashboardLayout";
 import Overview from "./pages/Overview";
 import AuraPathMap from "./pages/AuraPathMap";
@@ -6,20 +6,16 @@ import Analytics from "./pages/Analytics";
 import SystemHealth from "./pages/SystemHealth";
 import Privacy from "./pages/Privacy";
 
-// View components - placeholder functions removed, using imports from pages/
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<Overview />} />
-          <Route path="aurapath" element={<AuraPathMap />} />
-          <Route path="analytics" element={<Analytics />} />
-          <Route path="health" element={<SystemHealth />} />
-          <Route path="privacy" element={<Privacy />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<DashboardLayout />}>
+        <Route index element={<Overview />} />
+        <Route path="aurapath" element={<AuraPathMap />} />
+        <Route path="analytics" element={<Analytics />} />
+        <Route path="health" element={<SystemHealth />} />
+        <Route path="privacy" element={<Privacy />} />
+      </Route>
+    </Routes>
   );
 }
